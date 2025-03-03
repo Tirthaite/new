ae01c7692892f2243bcc4443e5110265a56a4372
Tirthaite <chudasamatirth83@gmail.com>
2025-03-03 16:42:04 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div style={{ textAlign: "center", marginTop: "50px", fontSize: "24px" }}>
+      <h1>Hello World</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
